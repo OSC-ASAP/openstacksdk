71c891335e571e809a12f3fbe6fa03c21815be32
choieastsea <choieastsea@gmail.com>
2024-09-02 19:31:35 +0900

    fix: quota field

--- openstack/dns/v2/quota.py
+++ openstack/dns/v2/quota.py
@@ -1,16 +1,26 @@
 from openstack.dns.v2 import _base
+from openstack import resource
 
 
-class Quota(_base.Resource):  # _base.Resource is a class from openstack/dns/v2/_base.py
+class Quota(_base.Resource):
     """DNS Quota Resource"""
 
     base_path = "/quotas"
 
     # capabilities
-    allow_list = True
     allow_fetch = True
     allow_commit = True
     allow_delete = True
+    allow_list = True
 
     # Properties
-    zones = _base.resource.Body("zones", type=int)
+    #: The ID of the project.
+    project = resource.URI("project")
+
+    api_export_size = resource.Body("api_export_size", type=int)
+
+    recordset_records = resource.Body("recordset_records", type=int)
+
+    zone_records = resource.Body("zone_records", type=int)
+
+    zones = resource.Body("zones", type=int)
